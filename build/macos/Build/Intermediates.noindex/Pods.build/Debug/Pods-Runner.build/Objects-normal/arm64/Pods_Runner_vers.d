c4b39d97232de70e5b868949b678648d183134e2
Chris Kepinski <90311657+cjkepinsky@users.noreply.github.com>
2024-12-09 10:04:40 +0100

    initial buils and run of the app

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods_Runner_vers.d
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods_Runner_vers.d
@@ -1,3 +1,3 @@
 dependencies: \
-  /Users/krzysztof/git/any_call_assistant/flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Users/krzysztof/git/any_call_assistant/flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
+  /Users/krzysztof/git/TalkExplainer_flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
+  /Users/krzysztof/git/TalkExplainer_flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/speech_to_text_macos.build/Objects-normal/arm64/speech_to_text_macos_vers.d
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/speech_to_text_macos.build/Objects-normal/arm64/speech_to_text_macos_vers.d
@@ -1,3 +1,3 @@
 dependencies: \
-  /Users/krzysztof/git/any_call_assistant/flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/speech_to_text_macos.build/module.modulemap \
-  /Users/krzysztof/git/any_call_assistant/flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/speech_to_text_macos.build/DerivedSources/speech_to_text_macos_vers.c
+  /Users/krzysztof/git/TalkExplainer_flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/speech_to_text_macos.build/module.modulemap \
+  /Users/krzysztof/git/TalkExplainer_flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/speech_to_text_macos.build/DerivedSources/speech_to_text_macos_vers.c
